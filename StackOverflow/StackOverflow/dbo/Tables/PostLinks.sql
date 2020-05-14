fdc94f0836879f4eb96a529237866656b60c5e8f
student <enquiries@dlmconsultants.com>
2020-05-14 18:37:05 +0000

    This should break the build

--- StackOverflow/StackOverflow/dbo/Tables/PostLinks.sql
+++ StackOverflow/StackOverflow/dbo/Tables/PostLinks.sql
@@ -2,7 +2,7 @@
     [Id]            INT      NOT NULL,
     [CreationDate]  DATETIME NOT NULL,
     [PostId]        INT      NOT NULL,
-    [RelatedPostId] INT      NOT NULL,
+    [RelatedPostId] INT      NOT NULL
     [LinkTypeId]    INT      NOT NULL,
     CONSTRAINT [PK_PostLinks__Id] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
